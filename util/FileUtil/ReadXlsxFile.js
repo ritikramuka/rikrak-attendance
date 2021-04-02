fd75f0c3eaae5983627c16eaa7333b3bd0fc9ea3
ritikramuka <ritik.ramuka@gmail.com>
2021-04-03 04:13:59 +0530

    update

--- util/FileUtil/ReadXlsxFile.js
+++ util/FileUtil/ReadXlsxFile.js
@@ -3,7 +3,17 @@ const XLSX = require("xlsx");
 
 function readXlsxFile(fileName) {
     var workbook = XLSX.readFile(fileName);
-    console.table(workbook.SheetNames[0]);
+    var sheet_name_list = workbook.SheetNames;
+
+
+    var worksheet = workbook.Sheets[sheet_name_list[0]];
+    var range = XLSX.utils.decode_range(worksheet['!ref']);
+    for (let R = range.s.r + 2; R <= range.e.r; R++) {
+        var cell_address = { c: 1, r: R };
+        var cell_ref = XLSX.utils.encode_cell(cell_address);
+        var email = worksheet[cell_ref].v;
+        console.log(email);
+    }
 }
 
 module.exports = {

985502b1a22e4bfb664394b85f630f4ba164880c
ritikramuka <ritik.ramuka@gmail.com>
2021-04-03 18:24:52 +0530

    update

--- util/FileUtil/ReadCSVFile.js
+++ util/FileUtil/ReadCSVFile.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 
-const Attendees = [];
-
-function readCSVFile(fileName) {
+function readCSVFile(fileName, AttendeesInfo) {
     fs.readFile(fileName, 'utf16le', function (err, data) {
         if (err)
             return;
@@ -11,8 +9,8 @@ function readCSVFile(fileName) {
             if (emailRegexp.test(dataArray[i]) == true) {
                 const userEmail = dataArray[i];
                 const timeStamp = dataArray[i - 1];
-                if ((Number)(timeStamp.split('m')[0]) >= 2)
-                    Attendees.push(userEmail);
+                if((Number)(timeStamp.split('m')[0]) >= 2)
+                    AttendeesInfo.push(userEmail); 
             }
         }
     })
@@ -22,5 +20,4 @@ const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-
 
 module.exports = {
     fn: readCSVFile,
-    data: readCSVFile,
 }

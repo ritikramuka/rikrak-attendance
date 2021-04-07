d48b89d8c6519e9209fe1cd7e712e3eaa6d44a92
Ritik Ramuka <ritik.ramuka@gmail.com>
2021-04-08 00:52:43 +0530

    packaged for win

--- util/FileUtil/ReadCSVFile.js
+++ util/FileUtil/ReadCSVFile.js
@@ -3,14 +3,17 @@ const fs = require('fs');
 function readCSVFile(fileName, AttendeesInfo) {
     const data = fs.readFileSync(fileName, 'utf16le');
     var dataArray = data.split(/\r?\n?\t/);
+    console.log(dataArray);
     for (let i = 0; i < dataArray.length; i++) {
         if (emailRegexp.test(dataArray[i]) == true) {
             const userEmail = dataArray[i];
             const timeStamp = dataArray[i - 1];
+            console.log(userEmail, timeStamp);
             if ((Number)(timeStamp.split('m')[0]) >= 50) //change time as per college requirements
                 AttendeesInfo.push(userEmail);
         }
     }
+    console.log(AttendeesInfo)
 }
 
 const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
